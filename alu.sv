5911f6868d5e41760a7286dab59591a71797c169
Takashi Hatakeyama <t.hatakeyama@MacBook-Pro-4.local>
2019-11-20 13:19:02 +0900

    Add alu files

---
+++ alu.sv
@@ -0,0 +1,9 @@
+module alu(
+    input logic [31:0] in1, in2,
+    input logic [2:0] funct3,
+    input logic [6:0] funct7,
+    output logic [31:0] result,
+    output logic negative, zero
+);
+endmodule
+
---
+++ alu_test.sv
@@ -0,0 +1,7 @@
+// ALU testbench
+// iverilog -g 2012 -s alu_test *.sv && ./a.out
+module alu_test;
+    initial begin
+        $display("hello");
+    end
+endmodule
