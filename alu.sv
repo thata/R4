77b58223e333e2318b470492ecc7a9f46cd02e01
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2019-11-29 20:02:35 +0900

    Add sltu

--- alu.sv
+++ alu.sv
@@ -13,17 +13,17 @@
 // 1001: and
 
 module alu(
-    input logic signed [31:0] in1, in2,
+    input logic [31:0] in1, in2,
     input logic [3:0] op,
     input logic [2:0] funct3,
     input logic [6:0] funct7,
-    output logic signed [31:0] result,
+    output logic [31:0] result,
     output logic negative, zero
 );
-    logic signed [31:0] sraResult;
+    logic [31:0] sraResult;
 
-    // NOTE: 三項演算子の中で算術シフトを行うとなぜか論理シフトになってしまうため、ここに持ってきた
-    assign sraResult = in1 >>> in2;
+    assign sraResult = $signed(in1) >>> in2;
+    assign sltuResult = (in1 < in2) ? 32'b1 : 32'b0;
 
     assign result = (op == 4'b0000) ? (in1 + in2)  :  // plus
                     (op == 4'b0001) ? (in1 - in2)  :  // minus
@@ -34,7 +34,7 @@ module alu(
                     (op == 4'b0110) ? (in1 >> in2) :  // srl (shift right logical)
                     (op == 4'b0111) ? sraResult    :  // sra (shift right arithmetic)
                     (op == 4'b0011) ? 32'bX        :  // slt 予定地
-                    (op == 4'b0100) ? 32'bX           // sltu 予定地
+                    (op == 4'b0100) ? sltuResult      // sltu 予定地
                                     : 32'hxxxxxxxx;
     assign negative = result[31];
     assign zero = ~|result;
