394fc69e3997d229174ad678f98e34a9bac3e4e9
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2019-12-12 20:40:39 +0900

    alu_controller を追加

--- decoder.sv
+++ decoder.sv
@@ -8,6 +8,18 @@ module decoder(
     output logic branch
 );
     logic [6:0] opCode;
+    logic [2:0] funct3;
+    logic [6:0] funct7;
+
+    // 00: ADD, 01: SUB, 10: Funct, 11: ---
+    logic [1:0] preAluOp;
+
+    alu_controller ac(
+        preAluOp,
+        funct3,
+        funct7,
+        aluOp
+    );
 
     assign opCode = instr[6:0];
 
@@ -30,6 +42,34 @@ module decoder(
     assign branch = (opCode === 7'b1100011) ? 1'b1 // B type
                                             : 1'b0;
 
-    assign aluOp = (opCode == 7'b1100011) ? 4'b0001 // beq => sub
-                                          : 4'b0000;
+    assign funct3 = instr[14:12];
+    assign funct7 = instr[31:25];
+    assign preAluOp = (opCode == 7'b1100011) ? 2'b01 : // B type => sub
+                      (opCode == 7'b0000011) ? 2'b00 : // lw => add
+                      (opCode == 7'b0100011) ? 2'b00   // sw => add
+                                             : 2'b10;  // funct
+endmodule
+
+module alu_controller(
+    input logic [1:0] preAluOp,
+    input logic [2:0] funct3,
+    input logic [6:0] funct7,
+    output logic [3:0] aluOp
+);
+
+    logic [9:0] funct;
+
+    assign funct = {funct7, funct3};
+
+    always_comb begin
+        case(preAluOp)
+            2'b00: aluOp = 4'b0000; // add
+            2'b01: aluOp = 4'b0001; // sub
+            default: case(funct)
+                10'b0000000000: aluOp = 4'b0000;
+                10'b0000000000: aluOp = 4'b0000;
+                default: aluOp = 4'bXXXX;
+            endcase
+        endcase
+    end
 endmodule
