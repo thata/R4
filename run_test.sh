1444a90db9a887a53631a293589467ed4a3c8d9d
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2020-01-11 06:46:50 +0900

    connect test_rom

--- run_test.sh
+++ run_test.sh
@@ -4,3 +4,4 @@ iverilog -g 2012 -s cpu_test *.sv && ./a.out
 iverilog -g 2012 -s decoder_test *.sv && ./a.out
 iverilog -g 2012 -s immgen_test *.sv && ./a.out
 iverilog -g 2012 -s regfile_test *.sv && ./a.out
+iverilog -g 2012 -s top_test *.sv && ./a.out
--- top_test.sv
+++ top_test.sv
@@ -1,6 +1,6 @@
 module top_test();
     logic clk = 0;
-    logic n_reset;
+    logic n_reset = 1;
     logic [31:0] instr;
     logic [31:0] readData;
     logic [31:0] result;
@@ -21,14 +21,43 @@ module top_test();
         we
     );
 
+    test_rom _rom(
+        instrAddr,
+        instr
+    );
+
     initial begin
-        $display("hello");
+        n_reset = 0;
+        #10;
+        n_reset = 1;
     end
 
     always #5 begin
+        $display("pc %x", instrAddr);
+        $display("instr %x", instr);
+        $display("result %x", result);
+
         clk <= !clk;
         
-        if ($time > 1000000)
+        if ($time > 1000)
             $finish;
     end
+endmodule
+
+module test_rom(
+    input logic [31:0] addr,
+    output logic [31:0] dout
+);
+    always_comb begin
+        case(addr)
+            32'h0000: dout = addi(5'd1, 5'd0, 12'b0);
+            32'h0004: dout = addi(5'd1, 5'd0, 12'b1);
+            32'h0008: dout = addi(5'd1, 5'd1, 12'b1);
+            32'h000c: dout = jal(5'd0, -2);
+            default:  dout = addi(5'b0, 5'b0, 5'b1);
+        endcase
+    end
+endmodule
+
+module data_ram();
 endmodule
